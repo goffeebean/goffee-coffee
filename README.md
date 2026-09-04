7b468ddc7b6345f7fe77897f135ffec668e622ae
Goffeebean <25123973-goffeebean@users.noreply.gitlab.com>
2026-09-04 11:34:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Amigoscode Inspired REST API
+
+Alas, it's been a couple of months since I've worked with Java, so I thought I would speed run through this tutorial
+from Amigoscode. See his video from 2025 here: https://www.youtube.com/watch?v=Cw0J6jYJtzw
+
+Amigoscode video is definitely a review of the basics, but he does go somewhere interesting here. At the end of the
+tutorial, he creates this Java/SpringBoot API with PostgresQL and OpenAI integration. Personally, I'd rather use ollama,
+and I'll use that here instead.
---
+++ src/main/java/com/goffeebean/CoffeeApplication.java
@@ -0,0 +1,21 @@
+package com.goffeebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+}
