e0839692ccd436e6808792e682261fa22f4795fc
Goffeebean <25123973-goffeebean@users.noreply.gitlab.com>
2026-09-05 17:12:23 -0400

    Feat: Added RestController.java

---
+++ src/main/java/com/goffeebean/controller/RoastController.java
@@ -0,0 +1,50 @@
+package com.goffeebean.controller;
+
+import com.goffeebean.dto.RoastRequest;
+import com.goffeebean.dto.RoastResponse;
+import com.goffeebean.service.RoastService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/roasts")
+public class RoastController {
+    private final RoastService roastService;
+
+    public RoastController (RoastService roastService){
+        this.roastService = roastService;
+    }
+
+    @GetMapping
+    public List<RoastResponse> getListOfRoasts(){
+        return roastService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public RoastResponse getRoastById(@PathVariable Long id){
+        return roastService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<RoastResponse> createRoast(@Valid @RequestBody
+                                                     RoastRequest request) {
+        RoastResponse response = roastService.create(request);
+        URI location = URI.create("/api/v1/roasts/" + response.id());
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public RoastResponse updateRoastById(@PathVariable Long id, @Valid @RequestBody RoastRequest request){
+        return roastService.update(id,request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRoastById(@PathVariable Long id){
+        roastService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
